edf862e6d60e5cd2074a6b6011d804cfe2b4bcea
timichal <michal@zlatkovsky.cz>
2019-07-02 11:56:28 +0200

    ugly defaults

--- sloupek.js
+++ sloupek.js
@@ -6,22 +6,13 @@ var pickr = Pickr.create({
   lockOpacity: true,
   comparison: false,
   swatches: [
-    'rgba(244, 67, 54, 1)',
-    'rgba(233, 30, 99, 1)',
-    'rgba(156, 39, 176, 1)',
-    'rgba(103, 58, 183, 1)',
-    'rgba(63, 81, 181, 1)',
-    'rgba(33, 150, 243, 1)',
-    'rgba(3, 169, 244, 1)',
-    'rgba(0, 188, 212, 1)',
-    'rgba(0, 150, 136, 1)',
-    'rgba(76, 175, 80, 1)',
-    'rgba(139, 195, 74, 1)',
-    'rgba(205, 220, 57, 1)',
-    'rgba(255, 235, 59, 1)',
-    'rgba(255, 193, 7, 1)'
+    "#56FF00",
+    "#0010FF",
+    "#6A7E25",
+    "#FF00EF",
+    "#806E28"
   ],
-
+  default: "#56FF00",
   components: {
     // Main components
     preview: true,
@@ -47,7 +38,5 @@ var pickr = Pickr.create({
 });
 
 pickr.on("init", function(instance) {
-  console.log("yay")
-  document.getElementsByClassName("pcr-app")[0].style.margin = "0 auto";
   document.getElementsByClassName("pcr-app")[0].style.width = "100%";
 });
