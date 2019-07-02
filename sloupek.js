dad19e00c428f047a10ef738b1b2f64904f5d7ee
timichal <michal@zlatkovsky.cz>
2019-07-02 12:49:20 +0200

    demo

--- sloupek.js
+++ sloupek.js
@@ -1,3 +1,5 @@
+var id = String(Date.now()) + Math.round(Math.random() * 1000);
+
 var pickr = Pickr.create({
   el: "#picker",
   theme: "classic",
@@ -37,6 +39,17 @@ var pickr = Pickr.create({
   }
 });
 
-pickr.on("init", function(instance) {
+pickr.on("init", function (instance) {
   document.getElementsByClassName("pcr-app")[0].style.width = "100%";
 });
+
+pickr.on("save", function (inpColor) {
+  var color = inpColor.toRGBA().toString(0);
+  var age = document.getElementById("picker-age").value || "N/A";
+  var gender = document.querySelector("input[name='picker-gender']:checked") ? document.querySelector("input[name='picker-gender']:checked").value : "N/A";
+  console.log(age, gender)
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://qk4458r8ag.execute-api.eu-central-1.amazonaws.com/default/sloupek-colorpicker");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({ "id": id, "color": color, "age": age, "gender": gender }));
+})
